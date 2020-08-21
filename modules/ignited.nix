c22feee4a65b1d7e2849c253d8cf8ae0582f7a2d
Marco Munizaga <git@marcopolo.io>
2020-08-20 17:05:35 -0700

    Changes

--- modules/ignited.nix
+++ modules/ignited.nix
@@ -25,12 +25,16 @@ in {
       serviceConfig = {
         User = "root";
         WorkingDirectory = "${pkgs.containerd}/bin";
+        ExecStartPre = ''
+        ${pkgs.bash}/bin/bash -c 'mkdir -p /opt/cni && ln -sf ${pkgs.cni-plugins}/bin /opt/cni/;'
+        '';
         ExecStart = [
           # "${pkgs.coreutils}/bin/pwd | echo"
           # "${pkgs.coreutils}/bin/ls ${pkgs.containerd}/bin | echo"
           # ''${pkgs.bash}/bin/bash -c "${ignite}/bin/ignited daemon"''
-          ''
-            ${pkgs.bash}/bin/bash -c "PATH=${pkgs.containerd}/bin:$PATH ${ignite}/bin/ignited daemon"''
+          # ''
+          #   ${pkgs.bash}/bin/bash -c "PATH=${pkgs.containerd}/bin:$PATH ${ignite}/bin/ignited daemon"''
+          ''${ignite}/bin/ignited daemon --log-level trace''
         ];
         ExecReload = [ "" "${pkgs.procps}/bin/kill -s HUP $MAINPID" ];
       };
--- shell.nix
+++ shell.nix
@@ -1,24 +1,37 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ sources ? import ./nix/sources.nix
+, pkgs ? import sources.nixpkgs                  # and use them again!
+    {
+      overlays = [
+        (_: pkgs:
+          {
+            niv = import sources.niv { }; # use the sources :)
+          }
+        )
+      ];
+      config = { };
+    }
+}: # import the sources
 let ignite = import ./ignite.nix { inherit pkgs; };
 in pkgs.mkShell {
-  CNI_PATH = "${pkgs.cni-plugins}/bin";
-  FOOO = "FOOO";
-  buildInputs = [
-    # pkgs.bash
-    # pkgs.coreutils
-    pkgs.containerd
-    # pkgs.openssh
-    # pkgs.e2fsprogs
-    # pkgs.gnutar
-    # pkgs.iptables
-    # pkgs.devicemapper
-    # pkgs.mount
-    pkgs.runc
-    pkgs.hello
-    ignite
-    pkgs.git
-    pkgs.binutils
-    pkgs.cni
-    pkgs.cni-plugins
-  ];
+CNI_PATH = "${pkgs.cni-plugins}/bin";
+FOOO = "FOOO";
+buildInputs = [
+pkgs.umount
+pkgs.bash
+pkgs.coreutils
+pkgs.containerd
+pkgs.openssh
+pkgs.e2fsprogs
+pkgs.gnutar
+pkgs.iptables
+pkgs.devicemapper
+pkgs.mount
+pkgs.runc
+pkgs.hello
+ignite
+pkgs.git
+pkgs.binutils
+pkgs.cni
+pkgs.cni-plugins
+];
 }

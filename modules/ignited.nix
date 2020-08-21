64c788e470c63ccdc8a2f080c099bb0003176114
Marco Munizaga <git@marcopolo.io>
2020-08-20 22:59:13 -0700

    Tweaks

--- modules/ignited.nix
+++ modules/ignited.nix
@@ -19,27 +19,27 @@ in {
 
   config = {
     users.groups.ignited = { };
+    services.containerd.enable = true;
 
     systemd.services.ignited = {
       wantedBy = optional cfg.enable "multi-user.target";
       serviceConfig = {
         User = "root";
-        WorkingDirectory = "${pkgs.containerd}/bin";
+        After = [ "containerd.service" ];
         ExecStartPre = ''
         ${pkgs.bash}/bin/bash -c 'mkdir -p /opt/cni && ln -sf ${pkgs.cni-plugins}/bin /opt/cni/;'
         '';
         ExecStart = [
           # "${pkgs.coreutils}/bin/pwd | echo"
           # "${pkgs.coreutils}/bin/ls ${pkgs.containerd}/bin | echo"
-          # ''${pkgs.bash}/bin/bash -c "${ignite}/bin/ignited daemon"''
-          # ''
-          #   ${pkgs.bash}/bin/bash -c "PATH=${pkgs.containerd}/bin:$PATH ${ignite}/bin/ignited daemon"''
-          ''${ignite}/bin/ignited daemon --log-level trace''
+          ''${pkgs.bash}/bin/bash -c "${ignite}/bin/ignited daemon"''
+          # ''${ignite}/bin/ignited daemon --log-level trace''
         ];
         ExecReload = [ "" "${pkgs.procps}/bin/kill -s HUP $MAINPID" ];
       };
 
       path = [
+        pkgs.umount
         pkgs.bash
         pkgs.coreutils
         pkgs.containerd
